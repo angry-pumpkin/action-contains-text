24d1d6b4ea7e60ec732f5804a1aff536cab20116
Peter Guest <peter.guest@cloudsmiths.co.za>
2022-10-19 15:45:02 +0200

    qaeqwr

--- dist/index.js
+++ dist/index.js
@@ -2822,12 +2822,19 @@ const core = __nccwpck_require__(186);
 const main = async () => {
 
     try {
-        
-        const inputString = core.getInput('input-string');
-        const inputPattern = core.getInput('input-pattern');
 
-        let regex = new RegExp(inputPattern);
-        let isMatched = regex.test(inputString);
+        const inputValue = core.getInput('input-value');
+        const searchValue = core.getInput('search-value');
+        let isMatched = false;
+        let matchValues = searchValue.split("|");
+
+        for (let i = 0; i < matchValues.length; i++) {
+
+            if(inputValue.includes(matchValues[i])) {
+                isMatched = true;
+                break;
+            }
+        }
 
         core.setOutput("is-matched", isMatched);
 
--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const core = require('@actions/core');
 const main = async () => {
 
     try {
-        
-        const inputString = core.getInput('input-string');
-        const inputPattern = core.getInput('input-pattern');
 
-        let regex = new RegExp(inputPattern);
-        let isMatched = regex.test(inputString);
+        const inputValue = core.getInput('input-value');
+        const searchValue = core.getInput('search-value');
+        let isMatched = false;
+        let matchValues = searchValue.split("|");
+
+        for (let i = 0; i < matchValues.length; i++) {
+
+            if(inputValue.includes(matchValues[i])) {
+                isMatched = true;
+                break;
+            }
+        }
 
         core.setOutput("is-matched", isMatched);
 
